af257356645c1927c62a595775b813bc2d8c0732
mirekkedzierski@outlook.com <mirekkedzierski@outlook.com>
2018-04-03 07:29:26 +0200

    Mobile- poprawiono mobilną wersję

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Projekt - zadanie  wykonanie albumu dla quantumlab
 ## Server localhost:3000
 
 Aby wystartować to node app
+
+###TODO
+-Dodać cookies
+-Dodać sidebar
--- website/js/main.js
+++ website/js/main.js
@@ -1,63 +1,98 @@
-
-
-var album = document.querySelectorAll(".album");
-let listItem = document.querySelectorAll(".collection-item");
 //Hide elements of Album list
+let listItem, link, showMore, showListMobile, windowWitdh, listItemContainer
 var albumListTogler = 6;
-var link = document.querySelectorAll("a");
-var showMore = document.getElementById("show");
-//Add event for ajax requests
 let d = true;
-album.forEach(element => {
-    element.addEventListener('mouseenter', () => {
-        element.classList.add = 'wad';
-        console.log("swa");
-    })
-});
-var container = document.getElementById("details");
-// container.innerHTML = "";
+//Add event for ajax requests
 window.onload = function () {
+    listItem = document.querySelectorAll(".collection-item");
+    link = document.querySelectorAll("a");
+    showMore = document.getElementById("show");
+    showListMobile = document.getElementById("showMobile");
+    console.log(listItemContainer);
+    windowWitdh = window.innerWidth;
+    loadAlbum();
+    //Add event to show more items on the list
+    if (windowWitdh > 600) {
+        showAlbumLinks(albumListTogler);
+        showHideList();
+    } else if (windowWitdh <= 600) {
+        d = !d;
+        showListMobile.classList.remove('hidden');
+        showHideList();
+        showAlbumLinks(albumListTogler - albumListTogler);
+        showListMobile.addEventListener('click', () => {
+            d = !d;
+            console.log(d);
+            showAlbumLinks(albumListTogler - albumListTogler);
+        })
+    }
 
-
+}
+/**
+ * Metoda - wczytuje treść do strony - Wybrany album.
+ * Nadaje linkom zdarzenie wywołujące obiekt XHR
+ * @param {NODE} link Element drzewa DOM - link
+ */
+function loadAlbum() {
     link.forEach(element => {
         element.addEventListener('click', (a) => {
             a.preventDefault();
+            if (windowWitdh <= 600) {
+                d = !d;
+                showAlbumLinks(0);
+            }
             loadData(element.getAttribute("href"));
         })
     });
-    //Add event to show more items on the list
-    showMore.addEventListener('click', (a)=>{
-        a.preventDefault();
-            a.target.classList.add("play");
-            if(d){
-                albumListTogler = 0;
-            }else{
-                albumListTogler = 6;
-            }
-            swowAllList();
-            d=!d;
-    })
-    
-    swowAllList();
 }
 /**
- *  Metoda shows full list of albums       
- * @param {Object} albumListTogler Array of nodes <li> 
+ * Metoda przełącza widok listy albumów niepełna/pełna
  */
-function swowAllList(){ 
-    console.log(albumListTogler + " --- " + d);
-    if(d){
-        for(var i =0; i< listItem.length-albumListTogler; i++ ){
+function showHideList() {
+    showMore.addEventListener('click', (a) => {
+        a.preventDefault();
+        console.log("show list");
+        a.target.classList.add("play");
+        if (d || windowWitdh <= 600) {
+            albumListTogler = 0;
+        } else {
+            albumListTogler = 6;
+
+        }
+        showAlbumLinks(albumListTogler);
+        d = !d;
+    })
+}
+
+/**
+ *  Metoda pokazuje/chowa listę albumów w panelu bocznym
+ * @param {Object} Number Array of nodes <li> 
+ */
+function showAlbumLinks(toHiede) {
+    console.log(toHiede + " --- " + d);
+    if (d) {
+        for (var i = 0; i < listItem.length - toHiede; i++) {
             listItem[i].classList.remove("hidden");
             listItem[i].classList.add("play");
         }
-    }else{
-        for(var i =albumListTogler; i< listItem.length; i++ ){
-            listItem[i].classList.add("hidden");
+    } else if (!d || windowWitdh <= 600) {
+        for (var i = toHiede; i < listItem.length; i++) {
             listItem[i].classList.add("play");
+            listItem[i].classList.add("hidden");
         }
-    }  
+    }
 }
+//Detekcja wielkości okna
+
+window.addEventListener('resize', resizeResponse);
+/**
+ * Metoda kontroluje szerokość okna przeglądarki
+ */
+function resizeResponse() {
+    windowWitdh = window.innerWidth;
+    window.location.reload('true');
+}
+
 /**
  * Ajax succes
  * Method handle of response from ajax
@@ -66,39 +101,7 @@ function handleSuccess() {
     const parseData = JSON.parse(this.responseText);
     generateView(parseData);
 }
-/**
- * Object recived from Http
- * @param {Object} data Object passed  
- */
-function generateView(data) {
-    console.log(data);
-    if(data){
-        container.innerHTML = 
-            `<div class = "col s12 m5 r-m-l play" >
-                <p>${data["description"]} </p></div>
-                <div class = "col s12 m7 offset-l1 offset-s0" >
-                    <img src = "${data["image"]}" alt = "${data["band"]} + ${data["title"]}" >
-                </div> 
-                <div class = "col s12 m3 l4" id = "ciekawostki" >
-                <strong>Ciekawostki</strong>
-                    <div>${generatePartialView(data.about)}</div>
-                </div> 
-            `;
-    }else{
-        container.innerHTML = `<div>Nie ma takich danych</div>`
-    }   
-}
-/**
- * Object passed by Ajax
- * @param {Object} date Part of data from main object 
- */
-function generatePartialView(date){
-    let info = '';
-    Object.keys(date).forEach(function(item){
-        info += `<div>${item}: <strong> ${date[item]}</strong></div>`;
-    });
-    return info;
-}
+
 /**
  * Ajax eror
  */
@@ -115,4 +118,42 @@ function loadData(adress) {
     xhr.onload = handleSuccess;
     xhr.onerror = handleErrors;
     xhr.send();
+}
+
+/**
+ * GENEROWANIE WIDOKÓW
+ */
+/**
+ * Object recived from Http
+ * @param {Object} data Object passed  
+ */
+function generateView(data) {
+    var container = document.getElementById("details");
+    console.log(data);
+    if (data) {
+        container.innerHTML =
+            `<div class = "col s12 m5 r-m-l play" >
+                <p>${data["description"]} </p></div>
+                <div class = "col s12 m7 offset-l1 offset-s0" >
+                    <img src = "${data["image"]}" alt = "${data["band"]} + ${data["title"]}" >
+                </div> 
+                <div class = "col s12 m3 l4" id = "ciekawostki" >
+                <strong>Ciekawostki</strong>
+                    <div>${generatePartialView(data.about)}</div>
+                </div> 
+            `;
+    } else {
+        container.innerHTML = `<div>Nie ma takich danych</div>`
+    }
+}
+/**
+ * Object passed by Ajax
+ * @param {Object} date Part of data from main object 
+ */
+function generatePartialView(date) {
+    let info = '';
+    Object.keys(date).forEach(function (item) {
+        info += `<div>${item}: <strong> ${date[item]}</strong></div>`;
+    });
+    return info;
 }
